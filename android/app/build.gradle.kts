e58879a9b9184369d2c85ae52cebe5bdc02e085d
Shakil Ahmmed <36266736+sXakil@users.noreply.github.com>
2026-09-08 01:58:11 +0600

    build: add material dependency and bump macOS deployment target to 12.0

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -38,6 +38,10 @@ android {
     }
 }
 
+dependencies {
+    implementation("com.google.android.material:material:1.14.0")
+}
+
 kotlin {
     compilerOptions {
         jvmTarget = org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_17
